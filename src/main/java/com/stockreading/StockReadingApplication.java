9f47b710391b81e044becc279d3227bf8821a6c1
binibean <bin4981@gmail.com>
2026-09-09 12:19:53 +0900

    chore: Spring Boot 프로젝트 초기 생성

---
+++ src/main/java/com/stockreading/StockReadingApplication.java
@@ -0,0 +1,13 @@
+package com.stockreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockReadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockReadingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockreading/StockReadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockReadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
